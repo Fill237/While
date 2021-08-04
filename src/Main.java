3507d5f44a4436422db14e44c08a71a995072e90
Fill237 <andreeva.mariya94@bk.ru>
2021-08-04 17:28:30 +0300

    counter_while

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Scanner src = new Scanner(System.in);
+            int x = src.nextInt();
+            int i = 0;
+            while (i<x) {
+                i++;
+                System.out.println(i);
+            }
+    }
+}
